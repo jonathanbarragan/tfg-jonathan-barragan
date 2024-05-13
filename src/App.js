20f5cfe1ba1db740a60ab1ba8cc576328faf66b6
Jonathan <jonathan.ba242@gmail.com>
2024-05-13 15:46:05 +0200

    leer json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import './App.css';
 import SearchBar from "./Components/SearchBar";
@@ -6,12 +6,14 @@ import { Login } from "./Components/Login";
 import { Cabecera } from "./Components/Cabecera";
 import appFirebase from '../src/FirebaseTFG'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { Register } from "./Components/Register";
 const auth = getAuth(appFirebase)
 
 
 function App() {
 
   // Estado para controlar la visibilidad del componente Login
+  const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const handleLoginClick = () => {
     // Cambiar el estado para mostrar el componente Login
@@ -20,42 +22,45 @@ function App() {
   const handleReturnHomeClick = () => {
     // Cambiar el estado para mostrar el componente Login
     setShowLogin(!showLogin);
+    setShowRegister(!showRegister);
   };
-  /* const [usuario, setUsuario] = useState(null);
-   onAuthStateChanged(auth, (usuarioFirebase)=>{
-     if(usuarioFirebase){
-       setUsuario(usuarioFirebase)
-     }else{
-       setUsuario(null)
-     }
-   })
- */
+  const handleRegisterClick = () => {
+    // Cambiar el estado para mostrar el componente Login
+    setShowRegister(!showRegister);
+  };
 
-
-
-  if (showLogin == false) {
+  if (showLogin == false && showRegister == false) {
     return (
-      
       <div className="App">
         <div className="home">
           <Image className="fondo" src='./Assets/JPG/fondo_app.jpg' />
           <Image className="logo" src="./Assets/PNG/logo.png" />
           <SearchBar placeholder="Escribe aqui..." ></SearchBar>
           <Button onClick={handleLoginClick} className="button-left" variant="primary" >Login</Button>
-          <Button className="button-right" variant="primary" >Register</Button>
+          <Button onClick={handleRegisterClick} className="button-right" variant="primary">Register</Button>
         </div>
       </div>
     );
   } else {
-    return (
-      <div className="web">
-        
-        <Cabecera/>
-        <Login />
-        <Button onClick={handleReturnHomeClick}> Return Home</Button>
-      </div>
-
-    );
+    if(showLogin == true){
+      return (
+        <div className="web">
+          <Cabecera/>
+          <Login />
+          <Button onClick={handleReturnHomeClick}> Return Home</Button>
+        </div>
+  
+      );
+    }
+    if(showRegister == true){
+      return(
+        <div className="web">
+          <Cabecera />
+          <Register />
+        </div>
+      )
+    }
+    
   }
 }
 
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,29 +1,41 @@
 import React, {useState} from "react"
 import "./Login.css";
 import appFirebase from "../FirebaseTFG";
+import { Register } from "./Register";
+import { Cabecera } from "./Cabecera";
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth(appFirebase)
 
 export const Login =() =>{
-    const [registered,setRegister] = useState(false);
     const [email, setEmail] = useState('');
     const [pass,setPass] = useState('');
     const handleSubmit =(e) => {
         e.preventDefault();
         console.log(email);
     }
-    return (
-        <div className="container">
-            <form onSubmit={handleSubmit} className="card card-body shadow-lg">
-                <label for ="email">Email </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type= "email" placeholder= "Enter your email" id ="email" name="email" className="textBox"/>
-                <label for ="password">Password </label>
-                <input value= {pass} onChange={(e) => setPass(e.target.value)} type= "password" placeholder= "********" id ="password" name="password" className="textBox"/>
-                <button type="submit" className="btn-login">Log In</button>
-                <button className="btn-register">Si no tienes una cuenta, Registrate</button>
-            </form>
-            
-        </div>
+    const [showRegister, setShowRegister] = useState(false);
+    const handleRegisterClick = () => {
+        // Cambiar el estado para mostrar el componente Register
+        setShowRegister(!showRegister);
+    };
+
+    if(showRegister == false){
+        return (
+            <div className="container">
+                <form onSubmit={handleSubmit} className="card card-body shadow-lg">
+                    <label for ="email">Email </label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type= "email" placeholder= "Enter your email" id ="email" name="email" className="textBox"/>
+                    <label for ="password">Password </label>
+                    <input value= {pass} onChange={(e) => setPass(e.target.value)} type= "password" placeholder= "********" id ="password" name="password" className="textBox"/>
+                    <button type="submit" className="btn-login">Log In</button>
+                    <button onClick={handleRegisterClick} className="btn-register">Si no tienes una cuenta, Registrate</button>
+                </form>
+            </div>
         )
+    }else{
+        return(
+            <Register />
+        )
+    }
 }
